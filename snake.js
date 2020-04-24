e268c4590099bcf8c0062eb9a1ebcf0fca016fbf
sunny odibo <sunnepazzy123@gmail.com>
2020-04-23 23:43:01 -0700

    my first commit

---
+++ snake.js
@@ -0,0 +1,22 @@
+
+function Snake(){
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1
+    this.yspeed = 0
+
+    this.update = function(){
+        this.x = this.x + this.xspeed;
+        this.y = this.y + this.yspeed;
+    }
+
+    this.show = function(){
+        fill(255)
+        rect(this.x, this.y, 10, 10)
+    }
+
+    this.dir = function(x, y){
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+}
